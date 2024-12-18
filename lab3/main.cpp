f650714f6068700aa889363179f3b27096e45c13
Alexa6748 <sandjieva.alex@gmail.com>
2024-12-18 16:50:37 +0300

    lab3

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -13,7 +13,6 @@ public:
 
     MyClass(int val = 0) : value(val) {}
 
-    // Оператор сравнения для сортировки
     bool operator<(const MyClass &other) const
     {
         return value < other.value;
@@ -25,37 +24,64 @@ public:
     }
 };
 
+// Функция для вывода размеров списка/вектора
+template <typename Container>
+void printSize(const Container &container, const std::string &name)
+{
+    std::cout << name << " size: " << container.size() << std::endl;
+}
+
+// Функция для вывода элементов списка/вектора
+template <typename Container>
+void printElements(const Container &container, const std::string &name)
+{
+    std::cout << name << " elements: ";
+    for (const auto &elem : container)
+    {
+        std::cout << elem.value << " ";
+    }
+    std::cout << std::endl;
+}
+
+// Специализация для списка пар (вывод пар элементов)
+template <typename T>
+void printElements(const std::list<std::pair<T, T>> &container, const std::string &name)
+{
+    std::cout << name << " pairs: ";
+    for (const auto &pair : container)
+    {
+        std::cout << "(" << pair.first.value << ", " << pair.second.value << ") ";
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
+    // Генерация случайных чисел
     std::mt19937 gen(std::random_device{}());
     std::uniform_int_distribution<int> dist(1, 100);
-
-    // Создание вектора v1 (500-1000 элементов) со случайными значениями
     std::uniform_int_distribution<int> size_dist(500, 1000);
+
+    // Пункт 1: Создание вектора v1
     int size_v1 = size_dist(gen);
     std::vector<MyClass> v1(size_v1);
     for (auto &elem : v1)
-    {
         elem.value = dist(gen);
-    }
 
-    // Пункт 2: Создаём вектор v2 из последних 200 элементов вектора v1
-    int b = size_v1 - 200; // Начальная позиция копирования
-    if (b < 0)
-        b = 0;       // Если вектор меньше 200 элементов, начинаем с начала
-    int e = size_v1; // Конечная позиция копирования
+    // Пункт 2: Создание вектора v2 (последние 200 элементов или диапазон [b, e))
+    int b = size_v1 - 200;
+    b = std::max(0, b); // Убедимся, что b >= 0
+    int e = size_v1;    // Конец диапазона
     std::vector<MyClass> v21(v1.begin() + b, v1.begin() + e);
     std::cout << "Sizes vectors, point 2 last 200: v1 = " << v1.size() << ", v2 = " << v21.size() << "\n";
-
-    // Рассматриваем произвольные b и e
+    // Генерация произвольных b и e
     std::uniform_int_distribution<int> range_dist(0, size_v1 - 1);
-    b = range_dist(gen); // Случайная начальная позиция
-    e = range_dist(gen); // Случайная конечная позиция
+    b = range_dist(gen);
+    e = range_dist(gen);
     if (b > e)
-        std::swap(b, e); // Если b больше e, меняем их местами
-    if (e > size_v1)
-        e = size_v1; // Убеждаемся, что e не выходит за пределы
-    // Создаём вектор v2 на основе диапазона [b, e)
+        std::swap(b, e); // Гарантируем, что b <= e
+    e = std::min(e, size_v1); // Убедимся, что e <= size_v1
+
     std::vector<MyClass> v2(v1.begin() + b, v1.begin() + e);
     std::cout << "Sizes vectors, point 2 with random b and e: v1 = " << v1.size() << ", v2 = " << v2.size() << "\n";
 
@@ -79,33 +105,30 @@ int main()
 
     std::cout << "Sizes lists points 3-4: list1 = " << list1.size() << ", list2 = " << list2.size() << "\n";
 
-    // Удаляем перемещённые элементы из v1 и v2
+
+    // Удаление перемещённых элементов
     v1.erase(v1.begin() + b, v1.end());
     v2.erase(v2.begin(), v2.begin() + n);
 
-    // Пункт 6: Находим средний элемент в list1 и перегруппируем элементы
+    // Пункт 6: Средний элемент list1 и перегруппировка
     auto median_it = std::next(list1.begin(), list1.size() / 2);
     MyClass median_value = *median_it;
     list1.sort([median_value](const MyClass &a, const MyClass &b)
                { return a.value > median_value.value; });
-    std::cout << "Median value of list1: " << median_value.value << std::endl;
 
-    // Пункт 7: Удаляем нечётные элементы из list2
-    list2.remove_if([](const MyClass &elem)
-                    { return elem.value % 2 != 0; });
-
-    // Пункт 8: Создаем вектор v3 из общих элементов v1 и v2
-    std::sort(v1.begin(), v1.end(), [](const MyClass &a, const MyClass &b)
-              { return a.value < b.value; });
-    std::sort(v2.begin(), v2.end(), [](const MyClass &a, const MyClass &b)
-              { return a.value < b.value; });
-    std::vector<MyClass> v3;
-    std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(),
-                          std::back_inserter(v3));
-
+    std::cout << "Median value of list1: " << median_value.value <<  "\n";
     
+    // Пункт 7: Удаление нечётных элементов из list2
+    list2.remove_if([](const MyClass &elem) { return elem.value % 2 != 0; });
 
-    // Пункт 10: Формирование пар для v1 и v2 без приведения размеров
+    // Пункт 8: Вектор v3 из общих элементов v1 и v2
+    std::sort(v1.begin(), v1.end());
+    std::sort(v2.begin(), v2.end());
+    std::vector<MyClass> v3;
+    std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(v3));
+
+
+    // Пункт 10: Формирование пар для list1 и list2 без приведения размеров
     std::list<std::pair<MyClass, MyClass>> list3_10;
     auto it1_10 = list1.begin();
     auto it2_10 = list2.begin();
@@ -116,33 +139,20 @@ int main()
         ++it2_10;
     }
 
-    std::cout << "Sizes vectors point 10: v1 = " << v1.size() << ", v2 = " << v2.size()
-              << ", v3 = " << v3.size() << "\n";
+    // Вывод размеров и элементов
+    printSize(v1, "v1");
+    printSize(v2, "v2");
+    printSize(list1, "list1");
+    printSize(list2, "list2");
+    printSize(list3_10, "list3_10");
+    printElements(list1, "list1");
+    printElements(list2, "list2");
+    printElements(list3_10, "list3");
 
-    std::cout << "Sizes lists point 10: list1 = " << list1.size() << ", list2 = " << list2.size()
-              << ", list3 = " << list3_10.size() << "\n";
-
-    for (const auto &pair : list1)
-    {
-        std::cout << pair.value << " ";
-    }
-    std::cout << std::endl;
-    for (const auto &pair : list2)
-    {
-        std::cout << pair.value << " ";
-    }
-    std::cout << std::endl;
-    for (const auto &pair : list3_10)
-    {
-        std::cout << "(" << pair.first.value << ", " << pair.second.value << ") ";
-    }
-    std::cout << std::endl;
-    // Пункт 9: Приводим списки list1 и list2 к одинаковому размеру и формируем list3
+    // Пункт 9: Приведение list1 и list2 к одинаковому размеру, формирование list3
     size_t min_size = std::min(list1.size(), list2.size());
-    while (list1.size() > min_size)
-        list1.pop_back();
-    while (list2.size() > min_size)
-        list2.pop_back();
+    list1.resize(min_size);
+    list2.resize(min_size);
     std::list<std::pair<MyClass, MyClass>> list3;
     auto it1 = list1.begin();
     auto it2 = list2.begin();
@@ -153,27 +163,15 @@ int main()
         ++it2;
     }
 
-    // Вывод для проверки
-    std::cout << "Sizes vectors point 9: v1 = " << v1.size() << ", v2 = " << v2.size()
-              << ", v3 = " << v3.size() << "\n";
-
-    std::cout << "Sizes lists point 9: list1 = " << list1.size() << ", list2 = " << list2.size()
-              << ", list3 = " << list3.size() << "\n";
-
-    for (const auto &pair : list1)
-    {
-        std::cout << pair.value << " ";
-    }
-    std::cout << std::endl;
-    for (const auto &pair : list2)
-    {
-        std::cout << pair.value << " ";
-    }
-    std::cout << std::endl;
-    for (const auto &pair : list3)
-    {
-        std::cout << "(" << pair.first.value << ", " << pair.second.value << ") ";
-    }
+    // Вывод размеров и элементов
+    printSize(v1, "v1");
+    printSize(v2, "v2");
+    printSize(list1, "list1");
+    printSize(list2, "list2");
+    printSize(list3, "list3_10");
+    printElements(list1, "list1");
+    printElements(list2, "list2");
+    printElements(list3, "list3");
 
     return 0;
 }
